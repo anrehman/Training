52c37f916fbaa39eeb9c5da76426fc202fc36d22
Aniqa Rehman <arehman@makenai.es>
2018-02-27 15:15:11 +0100

    Initialize

---
+++ .history/skills/cloneroom_20180227145623.js
@@ -0,0 +1,10 @@
+//
+// Command: room
+//
+module.exports = function (controller) {
+
+    controller.hears(["room"], 'direct_message,direct_mention', function (bot, message) {
+        
+        
+    });
+}
